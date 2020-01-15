29d45845fbbbd6cb980826e7cc729ab155c8ac9a
AbdelI <homeworkoracle@protonmail.com>
2020-01-15 15:12:51 +0100

    get request works to get the array of users

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const helmet = require("helmet");
 const server = express();
-
+const userRouter = require("./users/userRouter");
+const postsRouter = require("./posts/postRouter");
 server.use(express.json());
 
 server.use(helmet());
 
 server.use(logger);
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use("/api/users", userRouter);
+
+server.use("/api/posts", postsRouter);
+
+// server.get("/", (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 //custom middleware
 var moment = require("moment");
@@ -24,6 +29,10 @@ function logger(req, res, next) {
   );
   next();
 }
+
+const validateUserId = (req, res, next) => {
+  const { id } = req.params.id;
+};
 
 // server.get("*", (req, res) => {
 //   res.json("something is happening");
