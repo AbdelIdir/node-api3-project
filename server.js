10f0a8d89927eb79cd4ab305a272eeb168a7eb09
AbdelI <homeworkoracle@protonmail.com>
2020-01-15 14:35:17 +0100

    -

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ server.use(express.json());
 
 server.use(helmet());
 
+server.use(logger);
 
-server.use( logger)
-
-
-server.get("/", (req, res, ) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
@@ -20,7 +18,9 @@ moment().format();
 
 function logger(req, res, next) {
   console.log(
-    `Request method: ${req.method} | Url:  /4500${req.url} | Date: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+    `Request method: ${req.method} | Url:  /4500${
+      req.url
+    } | Date: ${moment().format("MMMM Do YYYY, h:mm:ss a")}`
   );
   next();
 }
